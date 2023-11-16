daa49cb1e91992eb48079a3eb6b5bec5a2dec5f0
vaishnav singh <vaishnavsingh162>
2023-11-17 01:11:02 +0530

    new question solved

---
+++ Array/MoveZero.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class moveZeroe {
+    public static void main(String[] args) {
+        // Example usage
+        int[] nums = { 0, 2, 0, 3, 4, 0, 5 };
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void moveZeroes(int[] nums) {
+        if (nums.length <= 1) {
+            return; // No need to move elements for an array of length 0 or 1
+        }
+
+        int nonZeroIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex++] = nums[i];
+            }
+        }
+
+        while (nonZeroIndex < nums.length) {
+            nums[nonZeroIndex++] = 0;
+        }
+    }
+}
---
+++ Array/RotateArray.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class RotateArray {
+
+    static void reverse(int[] arr, int start, int end) {
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+
+        }
+
+    }
+
+    static void leftrotate(int[] arr, int d, int n) {
+        if (n == 0)
+            return;
+        d = d % n;
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+
+    }
+
+    public static void main(String args[]) {
+        int n = 7;
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+        int d = 3;
+
+        System.out.println("Before rotation:");
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+
+        leftrotate(arr, n, d);
+        System.out.println("After rotation:");
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+}
